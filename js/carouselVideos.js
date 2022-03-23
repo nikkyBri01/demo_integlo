c6e8910f6c2f4413423e23297bf9c9b5787fa3c3
nikkyBri01 <99978867+nikkyBri01@users.noreply.github.com>
2022-03-23 09:18:59 -0600

    acomodando lapsos

--- js/carouselVideos.js
+++ js/carouselVideos.js
@@ -1,16 +1,20 @@
-$('#myCarousel').on('slid.bs.carousel', function (e) {
-    var elemento = $('#myCarousel .carousel-item.active video').first();
-    if (elemento.prop("tagName") == "video") {
-      elemento.get(0).play();
-    }
- });
- 
- $('#myCarousel').bind('slide.bs.carousel', function (e) {  
-    let elemento = $('#myCarousel .carousel-item.active video').first();
-    if (elemento.prop("tagName") == "video") {
-      elemento.get(0).pause();
-    }
- });
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slid.bs.carousel', function () {
+  var elemento = $('myCarousel .carousel-item-active').first();
+  if (elemento.prop("tagName") == "video") {
+    elemento.get(0).play();
+    elemento.get(0).load();
+  }
+})
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  var elemento = $('myCarousel .carousel .slide .carousel-fade .carousel-item.active video').first();
+  if (elemento.prop("tagName") == "video") {
+    elemento.get(0).pause();
+  }
+})
+
 
 
 
