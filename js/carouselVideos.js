fc62a83ca11bedc1f65b0cf93880ea0b18598fe4
nikkyBri01 <99978867+nikkyBri01@users.noreply.github.com>
2022-03-25 14:50:22 -0600

    creando carrusel #3 para la sección de productos

--- js/carouselVideos.js
+++ js/carouselVideos.js
@@ -21,7 +21,42 @@ var carousel = new bootstrap.Carousel(myCarousel2, {
   pause: false
 })
 
+/*Carousel de productos botones*/
+function App() {}
 
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+};
+
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const slickList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const slick = track.querySelectorAll('.slick');
+
+    const slickWidth = slick[0].offsetWidth;
+    
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
+
+    track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+    btn.dataset.button == "buttom-prev" ? prevAction(leftPosition,slickWidth,track) : nextAction(leftPosition,trackWidth,listWidth,slickWidth,track)
+}
+
+let prevAction = (leftPosition,slickWidth,track) => {
+    if(leftPosition > 0) {
+
+        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
+    if(leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }
+}
 
 
 
