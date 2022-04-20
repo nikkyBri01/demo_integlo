349316da2768f421bf3a61fc070d87558d062edb
nikkyBri01 <99978867+nikkyBri01@users.noreply.github.com>
2022-04-20 12:23:43 -0600

    modificando carrusel de productos

---
+++ js/Carruseles.js
@@ -0,0 +1,33 @@
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slid.bs.carousel', function () {
+  var elemento = $('myCarousel .carousel-item-active carousel-item-start').first();
+  if (elemento.prop("tagName") == "video") {
+    elemento.get(0).currentTime = 0;
+    elemento.get(0).play();
+  }
+})
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  var elemento = $('myCarousel .carousel .slide .carousel-fade .carousel-item.active video').first();
+  if (elemento.prop("tagName") == "video") {
+    elemento.get(0).pause();
+  }
+})
+
+//Carousel inicial no se pause cuando el mouse pase sobre él
+var myCarousel2 = document.querySelector('.carousel')
+var carousel = new bootstrap.Carousel(myCarousel2, {
+  pause: false
+})
+
+//Flickity
+var flkty = new Flickity( '.main-carousel', {
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true,
+  prevNextButtons: false,
+  pageDots: false,
+  autoPlay: 2000,
+  pauseAutoPlayOnHover: true
+});
